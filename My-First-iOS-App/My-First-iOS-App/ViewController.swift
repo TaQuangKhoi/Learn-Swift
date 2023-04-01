50a8d4d975310ee925f059edb55be72d231d3072
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-01 11:11:08 +0700

    Dùng thử outlet

--- My-First-iOS-App/My-First-iOS-App/ViewController.swift
+++ My-First-iOS-App/My-First-iOS-App/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHello: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lblHello.text = "Hi !!!"
+        lblHello.textColor = UIColor.red
+    
     }
 
-
+    
+    
 }
 
