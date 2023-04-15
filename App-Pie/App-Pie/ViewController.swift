5fefb6649e0dd73f49a5bc697dc884507eb6442e
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-15 15:56:11 +0700

    Finished Outlet Collection

--- App-Pie/App-Pie/ViewController.swift
+++ App-Pie/App-Pie/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImgView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
