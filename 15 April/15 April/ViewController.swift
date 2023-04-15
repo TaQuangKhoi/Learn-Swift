087ac8caf1a2b3e0b92ab9a70ff035b82a8bcd64
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-15 08:04:29 +0700

    Create project for 15 April 2023

---
+++ 15 April/15 April/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  15 April
+//
+//  Created by BVU on 4/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
