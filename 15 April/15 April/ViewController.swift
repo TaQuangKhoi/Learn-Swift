e22cecb55014793ee9da01e143907155eda5e113
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-15 10:13:54 +0700

    Add Control

--- 15 April/15 April/ViewController.swift
+++ 15 April/15 April/ViewController.swift
@@ -4,16 +4,40 @@
 //
 //  Created by BVU on 4/15/23.
 //
+import AVFoundation
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgAnimal2: UIButton!
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+//    @IBAction func clickBtnCat(_ sender: Any) {
+        
+//        if let image = UIImage(named: "cat") {
+//            imgAnimal.setImage(image, for: UIControl.State.normal)
+//        }
+//    }
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "cat", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
