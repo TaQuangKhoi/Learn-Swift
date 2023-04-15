d634500cb0df83efc6d09622968b27cc9b45694e
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-15 16:46:00 +0700

    format code

--- App-Pie/App-Pie/ViewController.swift
+++ App-Pie/App-Pie/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var treeImgView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
-
+    
     var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
     let incorrectMovesAllowed = 7
     
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         newRound()
     }
-
+    
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
@@ -69,12 +69,18 @@ class ViewController: UIViewController {
     }
     
     func updateGameState() {
-      if currentGame.incorrectMovesRemaining == 0 {
-        totalLosses += 1
-      } else if currentGame.word == currentGame.formattedWord {
-        totalWins += 1
-      } else {
-        updateUI()
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+      for button in letterButtons {
+        button.isEnabled = enable
       }
     }
     
