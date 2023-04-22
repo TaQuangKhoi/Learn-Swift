fa9abc04ba864dd0848b07430f47413a5cdc97e0
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 12:52:57 +0700

    Add ability to go back to Red View

--- TrafficSegues/TrafficSegues/ViewController.swift
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+    
+    }
 
 
 }
