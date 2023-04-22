d248b0eac0584e860a20230ab3b38a6602853d9e
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 14:33:23 +0700

    go to White View with performSegue

--- TrafficSegues/TrafficSegues/Controllers/PurpleViewController.swift
+++ TrafficSegues/TrafficSegues/Controllers/PurpleViewController.swift
@@ -12,11 +12,18 @@ class PurpleViewController: UIViewController {
     var name : String!
 
     @IBOutlet weak var lblPurple: UILabel!
+    @IBOutlet weak var btnPerformSegue: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblPurple.text = name
+    }
+    
+
+    @IBAction func goToWhiteView(_ sender: Any) {
+        performSegue(withIdentifier: "purpleToYellowSegue" , sender: sender)
     }
     
 
