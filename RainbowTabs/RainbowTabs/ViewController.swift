f4d0177755d25657444d13dc6fabe6095b1dcd89
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 16:09:06 +0700

    Create 3 views more

--- RainbowTabs/RainbowTabs/ViewController.swift
+++ RainbowTabs/RainbowTabs/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = "!"
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        tabBarItem.badgeValue = nil
     }
 
 
