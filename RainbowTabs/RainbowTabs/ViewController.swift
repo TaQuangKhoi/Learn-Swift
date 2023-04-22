12dbcbdea935b2df863f6cabed178c507dec08eb
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 15:31:06 +0700

    Create project RainbowTabs

---
+++ RainbowTabs/RainbowTabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by BVU on 4/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
