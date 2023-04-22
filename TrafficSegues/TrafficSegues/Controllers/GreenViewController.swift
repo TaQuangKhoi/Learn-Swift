4a3706519531b1c12a88c793af90404744485a89
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 13:42:05 +0700

    Add GreenViewController

---
+++ TrafficSegues/TrafficSegues/Controllers/GreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GreenViewController.swift
+//  TrafficSegues
+//
+//  Created by BVU on 4/22/23.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
