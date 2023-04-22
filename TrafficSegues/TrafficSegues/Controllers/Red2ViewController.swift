25ea31db92cab293a40faec8051043bbc81fb5ae
Keios Starqua on Mac <khoi0941@gmail.com>
2023-04-22 14:01:59 +0700

    Change title of Green view with info from red View

---
+++ TrafficSegues/TrafficSegues/Controllers/Red2ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Red2ViewController.swift
+//  TrafficSegues
+//
+//  Created by BVU on 4/22/23.
+//
+
+import UIKit
+
+class Red2ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
