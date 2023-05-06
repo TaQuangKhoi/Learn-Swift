ad27b0bddb9f8322b0e2736933bcab0e495ab133
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-06 14:46:24 +0700

    Create prj AppLifeCycle

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by BVU on 5/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
