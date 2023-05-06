98d08634a18f4dfb546ee45c0943f0724ad0c6ba
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-06 10:04:37 +0700

    Add var to Question VIew COntroller

--- PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -8,6 +8,43 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var questionIndex = 0
+    
+    var questions: [Question] = [
+      Question(
+        text: "Which food do you like the most?",
+        type: .single,
+        answers: [
+          Answer(text: "Steak", type: .dog),
+          Answer(text: "Fish", type: .cat),
+          Answer(text: "Carrots", type: .rabbit),
+          Answer(text: "Corn", type: .turtle)
+        ]
+      ),
+    
+      Question(
+        text: "Which activities do you enjoy?",
+        type: .multiple,
+        answers: [
+          Answer(text: "Swimming", type: .turtle),
+          Answer(text: "Sleeping", type: .cat),
+          Answer(text: "Cuddling", type: .rabbit),
+          Answer(text: "Eating", type: .dog)
+        ]
+      ),
+    
+      Question(
+        text: "How much do you enjoy car rides?",
+        type: .ranged,
+        answers: [
+          Answer(text: "I dislike them", type: .cat),
+          Answer(text: "I get a little nervous", type: .rabbit),
+          Answer(text: "I barely notice them", type: .turtle),
+          Answer(text: "I love them", type: .dog)
+        ]
+      )
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
