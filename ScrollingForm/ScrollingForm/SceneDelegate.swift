ebbbe3c637c0c21ba61f12cd4f283f3409cd76ef
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-06 15:24:13 +0700

    Create prj ScrollingForm
    Test Scene Delegate

--- ScrollingForm/ScrollingForm/SceneDelegate.swift
+++ ScrollingForm/ScrollingForm/SceneDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  SceneDelegate.swift
-//  AppLifeCycle
+//  ScrollingForm
 //
 //  Created by BVU on 5/6/23.
 //
@@ -10,10 +10,9 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var viewController: ViewController?
+
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        viewController = window?.rootViewController as? ViewController
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
