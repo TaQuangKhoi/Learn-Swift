a462bf1282b165fd1dc62d372732b9f276b6322d
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-06 11:20:05 +0700

    Show Result Done

--- PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
     var responses: [Answer]
     
     init?(coder: NSCoder, responses: [Answer]) {
@@ -24,9 +26,40 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+//      let frequencyOfAnswers = responses.reduce(into: [:]) {
+//        (counts, answer) in counts[answer.type, default: 0] += 1
+//      }
+        let frequencyOfAnswers = responses.reduce(into:
+           [AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
     }
     
 
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+    
+    }
+    
     /*
     // MARK: - Navigation
 
