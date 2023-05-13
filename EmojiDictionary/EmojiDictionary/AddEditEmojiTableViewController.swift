c77289a226b0074c108dd7911b13e7f51888c235
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-13 15:15:13 +0700

    Design Done

--- EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class AddEditEmojiTableViewController: UITableViewController {
     
+    @IBOutlet weak var symbolTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var usageTextField: UITextField!
+    
     var emoji: Emoji?
     
     init?(coder: NSCoder, emoji: Emoji?) {
@@ -32,15 +37,15 @@ class AddEditEmojiTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 4
+//    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
