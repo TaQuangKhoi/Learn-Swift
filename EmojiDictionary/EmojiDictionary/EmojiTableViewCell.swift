789a6e18346fbb099a1c7b542498e152d081eb28
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-13 09:36:03 +0700

    Add update func

--- EmojiDictionary/EmojiDictionary/EmojiTableViewCell.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var symbolLabel: UIStackView!
+    @IBOutlet weak var symbolLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
@@ -23,5 +23,10 @@ class EmojiTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
 }
---
+++ EmojiDictionary/EmojiDictionary/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  EmojiDictionary
+//
+//  Created by BVU on 5/13/23.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
