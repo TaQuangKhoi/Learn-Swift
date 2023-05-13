26da9c51778bd1854cc9065fa941f32c4d0e4065
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-13 16:44:30 +0700

    Create entity Emoji and its att

--- EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
@@ -140,16 +140,16 @@ class AddEditEmojiTableViewController: UITableViewController {
     }
     */
 
-//    override func prepare(for segue: UIStoryboardSegue,
-//       sender: Any?) {
-//    
-//        guard segue.identifier == "saveUnwind" else { return }
-//    
-//        let symbol = symbolTextField.text!
-//        let name = nameTextField.text ?? ""
-//        let description = descriptionTextField.text ?? ""
-//        let usage = usageTextField.text ?? ""
-//        emoji = Emoji(symbol: symbol, name: name,
-//           description: description, usage: usage)
-//    }
+    override func prepare(for segue: UIStoryboardSegue,
+       sender: Any?) {
+
+        guard segue.identifier == "saveUnwind" else { return }
+
+        let symbol = symbolTextField.text!
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        let usage = usageTextField.text ?? ""
+        emoji = Emoji(symbol: symbol, name: name,
+           description: description, usage: usage)
+    }
 }
