548c2c406ff612bcbfbd1a59b176a47cbdd9e619
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-13 13:21:49 +0700

    Init prop emoji

--- EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class AddEditEmojiTableViewController: UITableViewController {
-
+    
+    var emoji: Emoji?
+    
+    init?(coder: NSCoder, emoji: Emoji?) {
+        self.emoji = emoji
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
