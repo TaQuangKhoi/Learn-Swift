816b3810667cad70b6bc628a386d7bbde214507f
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-20 08:42:59 +0700

    Delete all outlet from UI of new screen

---
+++ TaQuangKhoiGKSmartQuizz/TaQuangKhoiGKSmartQuizz/Screen3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Screen3ViewController.swift
+//  TaQuangKhoiGKSmartQuizz
+//
+//  Created by BVU on 5/20/23.
+//
+
+import UIKit
+
+class Screen3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
