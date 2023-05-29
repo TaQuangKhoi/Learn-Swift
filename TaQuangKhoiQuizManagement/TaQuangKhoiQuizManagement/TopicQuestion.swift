265892dfbb58bb06fa65d27d6de58487c4eef288
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-29 09:10:00 +0700

    Create file structs

---
+++ TaQuangKhoiQuizManagement/TaQuangKhoiQuizManagement/TopicQuestion.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  TaQuangKhoiQuizManagement
+//
+//  Created by BVU on 5/29/23.
+//
+
+import Foundation
+
+struct TopicQuestion {
+    var name: String
+    var Answer: Array<String>
+}
