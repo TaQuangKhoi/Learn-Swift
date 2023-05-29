fb4fb7f3a3520dd8984c798d04d49e074cc8c443
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-29 07:27:55 +0700

    Create project

---
+++ TaQuangKhoiQuizManagement/TaQuangKhoiQuizManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaQuangKhoiQuizManagement
+//
+//  Created by BVU on 5/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
