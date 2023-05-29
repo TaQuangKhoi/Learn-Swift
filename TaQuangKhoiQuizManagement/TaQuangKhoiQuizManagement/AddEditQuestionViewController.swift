07c1c1cd6409f1913e7749e8e55b626871f33278
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-29 10:02:57 +0700

    Create file and add outlet

---
+++ TaQuangKhoiQuizManagement/TaQuangKhoiQuizManagement/AddEditQuestionViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddEditQuestionViewController.swift
+//  TaQuangKhoiQuizManagement
+//
+//  Created by BVU on 5/29/23.
+//
+
+import UIKit
+
+class AddEditQuestionViewController: UIViewController {
+
+    @IBOutlet weak var questionNameTF: UITextField!
+    
+    @IBOutlet weak var answer1TF: UITextField!
+    
+    @IBOutlet weak var answer2TF: UITextField!
+    
+    @IBOutlet weak var answer3TF: UITextField!
+    
+    @IBOutlet weak var answer4TF: UITextField!
+    
+    @IBOutlet weak var rightAnswerIndex: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
