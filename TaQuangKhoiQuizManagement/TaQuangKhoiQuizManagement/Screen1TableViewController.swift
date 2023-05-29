fce6e87ee47d4068a407eac4ce865354ef895a46
Keios Starqua on Mac <khoi0941@gmail.com>
2023-05-29 09:54:43 +0700

    Delete topic

--- TaQuangKhoiQuizManagement/TaQuangKhoiQuizManagement/Screen1TableViewController.swift
+++ TaQuangKhoiQuizManagement/TaQuangKhoiQuizManagement/Screen1TableViewController.swift
@@ -87,17 +87,18 @@ class Screen1TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            topics.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     
     // Override to support rearranging the table view.
